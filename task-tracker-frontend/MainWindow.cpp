b150e406d6a8c0141668ce88744953ddb63ba3ea
Vladislav Kalinin <unkorunk@gmail.com>
2021-04-22 23:54:14 +1000

    Initial project #TL-2 #TL-3

---
+++ task-tracker-frontend/MainWindow.cpp
@@ -0,0 +1,22 @@
+#include "MainWindow.h"
+#include "./ui_MainWindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+
+    connect( ui->pushButton, SIGNAL(clicked()), this, SLOT(OnSayHello()) );
+}
+
+void MainWindow::OnSayHello() {
+    ui->label->setText("Hello World");
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
